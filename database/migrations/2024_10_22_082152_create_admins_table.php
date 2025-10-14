46c074438e81a98561dde0c2ca65966b00aa1912
clarenceevanw <clarenceevan0907@gmail.com>
2025-10-14 20:14:51 +0700

    fix: null link gmeet

--- database/migrations/2024_10_22_082152_create_admins_table.php
+++ database/migrations/2024_10_22_082152_create_admins_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('nrp');
             $table->string('name');
             $table->string('anonymous_name');
-            $table->string('link_gmeet');
+            $table->string('link_gmeet')->nullable();
             $table->string('lokasi_wawancara')->nullable();
 
             $table->foreign('division_id')->references('id')->on('divisions');
