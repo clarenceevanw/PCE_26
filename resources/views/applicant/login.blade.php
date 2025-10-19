a14acda5602132fee6b61323427798221f5f81ca
clarenceevanw <clarenceevan0907@gmail.com>
2025-10-19 16:53:12 +0700

    fix: login logo

--- resources/views/applicant/login.blade.php
+++ resources/views/applicant/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PCE 2026 | Login</title>
-    <link rel="icon" href="{{ asset('assets/logo.png') }}">
+    <link rel="icon" href="{{ asset('assets/logoAja.png') }}">
 
     {{-- TW Elements --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css" />
